3f9db27d755887468fd010493987ac32a5c873d4
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-05-27 12:21:13 +0530

    add useEffect on counter app

--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Button from "./common/Button";
 
 const CounterApp = () => {
     const [count, setCount] = useState(0);
@@ -6,25 +7,28 @@ const CounterApp = () => {
     const decrement = () => setCount(count - 1);
     const reset = () => setCount(0);
 
+    useEffect(() => {
+        console.log(`${count}`);
+    }, [count]);
+
     return (
         <>
             <div className="flex justify-center items-center pb-20">
                 <div className="max-w-[1140px] px-4 mx-auto flex flex-col justify-center items-center">
                     <div className="font-mono text-3xl">{count}</div>
                     <div className="gap-4 flex justify-center items-center mt-4">
-                        <button className="border-black border bg-black transition-all duration-200 ease-linear hover:bg-white hover:text-black text-white py-2 px-7 rounded-full font-mono text-lg cursor-pointer" onClick={decrement}>
-                            Decrease
-                        </button>
-                        <button className="border-black border bg-black transition-all duration-200 ease-linear hover:bg-white hover:text-black text-white py-2 px-7 rounded-full font-mono text-lg cursor-pointer" onClick={increment}>
-                            Increase
-                        </button>
+                        <Button onClick={decrement} text="Decrease" />
+                        <Button onClick={increment} text="Increase" />
                     </div>
-                    <button className={count === 0 ?
-                        "border-gray-400 border mx-auto flex mt-4 bg-gray-400 transition-all duration-200 ease-linear text-white py-2 px-7 rounded-full font-mono text-lg cursor-not-allowed"
-                        : "border-black border mx-auto flex mt-4 bg-black transition-all duration-200 ease-linear hover:border-red-500 hover:bg-red-500 text-white py-2 px-7 rounded-full font-mono text-lg cursor-pointer"}
-                        onClick={reset}>
-                        Reset
-                    </button>
+                    <Button
+                        onClick={reset}
+                        btnClass={
+                            count === 0
+                                ? "border-gray-400 bg-gray-400 hover:!bg-gray-400 hover:text-white !cursor-not-allowed"
+                                : "bg-black hover:border-red-500 hover:!bg-red-500 hover:text-white"
+                        }
+                        text="Reset"
+                    />
                 </div>
             </div>
         </>
