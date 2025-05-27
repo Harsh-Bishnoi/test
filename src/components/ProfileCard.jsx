ddc6a985d39ce006686239898604d738ae55a794
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-05-27 12:45:13 +0530

    add corousel component

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Card from './common/Card'
-import my from '../assets/images/svg/my.svg'
+import img_1 from '../assets/images/png/img-1.png'
 import Button from './common/Button'
 
 const ProfileCard = () => {
@@ -12,7 +12,7 @@ const ProfileCard = () => {
             <div className="flex justify-center items-center py-20">
                 <div className="max-w-[1140px] px-4">
                     <div div className="flex flex-wrap gap-6 justify-center" >
-                        <Card img={my} Name="Harsh Bishnoi" Bio="I'm Harsh Bishnoi, a frontend developer skilled in HTML, CSS, JavaScript, and React." Contact="8222033295" />
+                        <Card img={img_1} Name="Harsh Bishnoi" Bio="I'm Harsh Bishnoi, a frontend developer skilled in HTML, CSS, JavaScript, and React." Contact="8222033295" />
                     </div >
                     <Button onClick={ClickBtn} text="Click" />
                 </div>
