4ebcd53a627f4d44002b5488eeb2e9587d0004f7
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-05-27 12:27:08 +0530

    install tooltip

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -12,7 +12,7 @@ const ProfileCard = () => {
             <div className="flex justify-center items-center py-20">
                 <div className="max-w-[1140px] px-4">
                     <div div className="flex flex-wrap gap-6 justify-center" >
-                        <Card img={my} Name="Harsh Bishnoi" Bio="Front End Developer" Contact="8222033295" />
+                        <Card img={my} Name="Harsh Bishnoi" Bio="I'm Harsh Bishnoi, a frontend developer skilled in HTML, CSS, JavaScript, and React." Contact="8222033295" />
                     </div >
                     <Button onClick={ClickBtn} text="Click" />
                 </div>
